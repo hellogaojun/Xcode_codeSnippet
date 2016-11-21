e9f8940f3d76a73189db1ea698337e443bdad8c5
gaojun <2293209493@qq.com>
2016-11-21 22:01:20 +0800

    删除.sh文件

--- setup_snippets.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-mv ~/Library/Developer/Xcode/UserData/CodeSnippets ~/Library/Developer/Xcode/UserData/CodeSnippets.backup
-
-SRC_HOME = 'pwd'
-ln -s ${SRC_HOME}/codeSnippets ~/Library/Developer/Xcode/UserData/CodeSnippets
-
-echo "done"
